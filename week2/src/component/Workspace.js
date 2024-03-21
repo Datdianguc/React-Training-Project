c57d19da16efda6d9f22bc8047020e038a60f31b
Datdianguc <tiendat989@gmail.com>
2024-03-21 15:56:28 +0700

    3:56PM 21/3/2024

--- week2/src/component/Workspace.js
+++ week2/src/component/Workspace.js
@@ -1,34 +1,67 @@
 import React from "react";
 import "../totalcss/Workspace.css";
 export default class Workspace extends React.Component {
-  state = { count: 0, text: "", list: [] };
+  state = {
+    count: 0,
+    text: "",
+    list: [],
+  };
+
   handleChange = (event) => {
     this.setState({ text: event.target.value });
-    console.log(this.state.text);
   };
+
   handleSubmit = (event) => {
     event.preventDefault();
-    const NewItem = this.state.text;
-    this.setState({
-      count: this.state.count + 1,
-      list: [...this.state.list, NewItem],
-    });
-    console.log(this.state.list);
+    const newItem = this.state.text;
+    if (newItem.trim() !== "") {
+      this.setState((prevState) => ({
+        count: prevState.count + 1,
+        list: [
+          ...prevState.list,
+          { id: prevState.count + 1, name: newItem, checked: false },
+        ],
+        text: "",
+      }));
+    }
   };
+
+  handleCheckboxChange = (id) => {
+    this.setState((prevState) => ({
+      list: prevState.list.map((item) =>
+        item.id === id ? { ...item, checked: !item.checked } : item
+      ),
+    }));
+  };
+
   render() {
     return (
       <div className="typing">
         <form onSubmit={this.handleSubmit}>
           <input
+            value={this.state.text}
             className="pleasetype"
             placeholder="What need to be done?"
             onChange={this.handleChange}
-          ></input>
+          />
         </form>
         <div className="output">
-          {this.state.list.map((n) => {
-            return n;
-          })}
+          {this.state.list.map((item) => (
+            <div key={item.id}>
+              <input
+                type="checkbox"
+                checked={item.checked}
+                onChange={() => this.handleCheckboxChange(item.id)}
+              />
+              <label
+                style={{
+                  textDecoration: item.checked ? "line-through" : "none",
+                }}
+              >
+                {item.name}
+              </label>
+            </div>
+          ))}
         </div>
       </div>
     );
